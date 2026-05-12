9b733e8066e562ff48a0490d134ae36665801dff
bariulmunshi <bariulsoft@gmail.com>
2026-05-12 07:58:05 +0600

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+# ফাংশন ব্যবহার  করে List এর পাঁচটি সংখ্যাকে যোগ করার Program লেখ।
+numbers_list = [1, 2, 3, 4, 5]
+def sum_of_numbers(numbers):
+    total = 0
+    for number in numbers:
+        total= total+ number
+    return total
+
+print(sum_of_numbers(numbers_list))
