72a7969606d49d2bf16abd42dd93cc32c57b6ba7
bariulmunshi <bariulsoft@gmail.com>
2026-05-12 08:21:38 +0600

    added explainaiton

--- 1.py
+++ 1.py
@@ -10,6 +10,25 @@ def sum_of_numbers(numbers):
 print(sum_of_numbers(numbers_list))
 
 '''
+# code explaination: 
+# Step-1: List তৈরি হচ্ছে step-2: Function তৈরি হচ্ছে step-3: Function Call হচ্ছে
 #print(sum([1, 2, 3, 4, 5])) 
 # 2. ফাংশন ব্যবহার  করে ধনাত্মক  পূর্ণ সংখ্যার Factorial এর মান নির্ণয় করার program লেখ। 
-    
+# ফাংশন ব্যবহার করে ধনাত্মক পূর্ণ সংখ্যার Factorial নির্ণয়
+
+def factorial(number):
+    fact = 1
+
+    for i in range(1, number + 1):
+        fact = fact * i
+
+    return fact
+
+
+num = int(input("Take a positive integer: "))
+
+if num < 0:
+    print("Factorial is not positive number.")
+else:
+    result = factorial(num)
+    print("Factorial =", result)
