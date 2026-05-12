8b9b89ae6875ba3ac381d3e866a57918ce15b8d4
bariulmunshi <bariulsoft@gmail.com>
2026-05-12 08:08:53 +0600

    added

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
-# ফাংশন ব্যবহার  করে List এর পাঁচটি সংখ্যাকে যোগ করার Program লেখ।
+# 1. ফাংশন ব্যবহার  করে List এর পাঁচটি সংখ্যাকে যোগ করার Program লেখ।
+'''
 numbers_list = [1, 2, 3, 4, 5]
 def sum_of_numbers(numbers):
     total = 0
@@ -7,3 +8,8 @@ def sum_of_numbers(numbers):
     return total
 
 print(sum_of_numbers(numbers_list))
+
+'''
+#print(sum([1, 2, 3, 4, 5])) 
+# 2. ফাংশন ব্যবহার  করে ধনাত্মক  পূর্ণ সংখ্যার Factorial এর মান নির্ণয় করার program লেখ। 
+    
